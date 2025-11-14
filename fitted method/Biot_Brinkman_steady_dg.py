7c29c684691d4c9d1c6748ee69d8ee7d91599274
lushuming <lushuming1827@163.com>
2025-11-14 22:49:31 +0800

    Biot-Brinkman

--- fitted method/Biot_Brinkman_steady_dg.py
+++ fitted method/Biot_Brinkman_steady_dg.py
@@ -1,21 +1,27 @@
 from netgen.occ import *
+from netgen.geom2d import SplineGeometry
 from ngsolve import *
 from ngsolve.webgui import Draw
 from math import pi,e
 from numpy import linspace
+import numpy as np
 
 
 def Stress(strain):
     return 2*mu*strain + lam*Trace(strain)*Id(2)
 
-def solve_biotBrinkman_steady_dg(h, order_eta, order_u,order_p, fe, fm,fp, exact_eta, exact_u, exact_p, mu,lam,beta_eta,beta_u,alpha,K):
+def solve_biotBrinkman_steady_dg(h, order_eta, order_u,order_p, fe, fm,fp, exact_eta, exact_u, exact_p, mu,lam,beta_eta,beta_u,gamma_p,alpha,K):
 
     # 1. Construct the mesh
-    mesh = Mesh(unit_square.GenerateMesh(maxh=h))
+    # mesh = Mesh(unit_square.GenerateMesh(maxh=h))
+    square = SplineGeometry()
+    square.AddRectangle((0, 0), (1, 1),bc=1)
+    ngmesh = square.GenerateMesh(maxh=h, quad_dominated=False)
+    mesh = Mesh(ngmesh)
 
-    E = VectorL2(mesh, order=order_eta, dirichlet=".*", dgjumps=True) # space for displacement
-    U = VectorL2(mesh, order=order_u, dirichlet=".*", dgjumps=True) # space for velocity
-    P = L2(mesh, order=order_p, dirichlet=".*", dgjumps=True) # space for pressure
+    E = VectorL2(mesh, order=order_eta, dirichlet=[], dgjumps=True) # space for displacement
+    U = VectorL2(mesh, order=order_u, dirichlet=[], dgjumps=True) # space for velocity
+    P = L2(mesh, order=order_p, dirichlet=[], dgjumps=True) # space for pressure
     fes = E*U*P
     (eta,u,p), (kxi,v,q) = fes.TnT()
     
@@ -31,8 +37,8 @@ def solve_biotBrinkman_steady_dg(h, order_eta, order_u,order_p, fe, fm,fp, exact
     jump_kxi = kxi - kxi.Other()
     
     
-    jump_p = p - p.Other()
-    jump_q = q - q.Other()
+    # jump_p = p - p.Other()
+    # jump_q = q - q.Other()
     mean_p = 0.5*(p + p.Other())
     mean_q = 0.5*(q + q.Other())
     
@@ -46,35 +52,38 @@ def solve_biotBrinkman_steady_dg(h, order_eta, order_u,order_p, fe, fm,fp, exact
     # Ae
     Ah += 2*mu*InnerProduct(strain_eta,strain_kxi)*dx + lam*div(eta)*div(kxi)*dx \
             - (InnerProduct(mean_stress_eta,jump_kxi) + InnerProduct(mean_stress_kxi,jump_eta) - beta_eta/h*InnerProduct(jump_eta,jump_kxi))*dx(skeleton=True) \
-            - (InnerProduct(Stress(Sym(Grad(eta)))*n,kxi) + InnerProduct(Stress(Sym(Grad(kxi)))*n,eta) - beta_eta/h*InnerProduct(kxi,eta))*ds(skeleton=True)
-    
+            - (InnerProduct(Stress(Sym(Grad(eta)))*n,kxi) + InnerProduct(Stress(Sym(Grad(kxi)))*n,eta) - beta_eta/h*InnerProduct(eta,kxi))*ds(skeleton=True)
+
     # Be
     Ah += -alpha*(div(kxi)*p*dx - mean_p*jump_kxi*n*dx(skeleton=True) - p*kxi*n*ds(skeleton=True))
-    
+
     # Am
-    Ah += 2*nu*InnerProduct(Grad(u),Grad(v))*dx \
-            - 2*nu*(InnerProduct(mean_dudn,jump_v) + InnerProduct(mean_dvdn,jump_u) - beta_u/h*InnerProduct(jump_u,jump_v))*dx(skeleton=True) \
-            - 2*nu*(InnerProduct(Grad(u)*n,v) + InnerProduct(Grad(v)*n,u) - beta_u/h*InnerProduct(u,v))*ds(skeleton=True) \
+    Ah += nu*InnerProduct(Grad(u),Grad(v))*dx \
+            - nu*(InnerProduct(mean_dudn,jump_v) + InnerProduct(mean_dvdn,jump_u) - beta_u/h*InnerProduct(jump_u,jump_v))*dx(skeleton=True) \
+            - nu*(InnerProduct(Grad(u)*n,v) + InnerProduct(Grad(v)*n,u) - beta_u/h*InnerProduct(u,v))*ds(skeleton=True) \
             + K*InnerProduct(u,v)*dx
-    
+
     # Bm 
     Ah += -div(v)*p*dx + mean_p*jump_v*n*dx(skeleton=True) + p*v*n*ds(skeleton=True)
-    
+
     # -Bm
     Ah += div(u)*q*dx - mean_q*jump_u*n*dx(skeleton=True) - q*u*n*ds(skeleton=True)
-    
+
     # M
     Ah += s0*p*q*dx
-    
+
     # -Be
     Ah += alpha*(div(eta)*q*dx - mean_q*jump_eta*n*dx(skeleton=True) - q*eta*n*ds(skeleton=True))
-    
+
+    Ah += gamma_p*h*h*grad(p)*grad(q)*dx
     Ah.Assemble()
 
     lh = LinearForm(fes)
+    # lh += fe*kxi*dx - InnerProduct(etaD,Stress(Sym(Grad(kxi)))*n)*ds(skeleton=True) + beta_eta/h*etaD*kxi*ds(skeleton=True) + alpha*pD*kxi*n*ds(skeleton=True)
     lh += fe*kxi*dx - InnerProduct(etaD,Stress(Sym(Grad(kxi)))*n)*ds(skeleton=True) + beta_eta/h*etaD*kxi*ds(skeleton=True)
-    lh += fm*v*dx - 2*nu*InnerProduct(uD,Stress(Sym(Grad(v)))*n)*ds(skeleton=True) + 2*nu*beta_u/h*uD*v*ds(skeleton=True) + v*n*pD*ds(skeleton=True) 
-    lh += fp*q*dx 
+    # lh += fm*v*dx - nu*InnerProduct(uD,Grad(v)*n)*ds(skeleton=True) + nu*beta_u/h*uD*v*ds(skeleton=True) + v*n*pD*ds(skeleton=True) 
+    lh += fm*v*dx - nu*InnerProduct(uD,Grad(v)*n)*ds(skeleton=True) + nu*beta_u/h*uD*v*ds(skeleton=True)
+    lh += fp*q*dx - alpha*q*etaD*n*ds(skeleton=True) - q*uD*n*ds(skeleton=True)
     lh.Assemble()
 
     gfu = GridFunction(fes)
@@ -91,7 +100,7 @@ def print_convergence_table(results):
     print("-" * 70)
     for i, (h,dofs,error_eta,error_u,error_p) in enumerate(results):
         if i == 0:
-            print(f"{h:8.4f} | {dofs:8d} | {error_eta:12.4e} |  {'-':>6} |{error_u:12.4e} | {'-':>6}| {error_p:12.4e} | {'-':>6}")
+            print(f"{h:8.4f} | {dofs:8d} | {error_eta:12.4e} | {'-':>6} |{error_u:12.4e}| {'-':>6}| {error_p:12.4e} | {'-':>6}")
         else:
             prev_h,_,prev_error_eta, prev_error_u,prev_error_p = results[i-1]
             rate_eta = (np.log(prev_error_eta) - np.log(error_eta)) / (np.log(prev_h) - np.log(h))
@@ -103,31 +112,41 @@ def print_convergence_table(results):
 
 # Define important parameters
 mu  = 10
-lam = 100
+lam = 10
 alpha = 1
-K = 10 # k^-1
+K = 1 # k^-1
 nu = 1
 s0 = 1e-2
 
 
 quad_mesh = True
 # Finite element space order
-order_eta = 1
-order_u = 1
-order_p = 0
+order_eta = 2
+order_u = 2
+order_p = 1
 
 # penalty parameters
-beta_eta = 100
-beta_u = 100 
+# alpha = 10 , beta_eta = 50, beta_u = 10
+beta_eta = 200
+beta_u = 200
+gamma_p = 0
 
 # Manufactured exact solution for monitoring the error
 eta_x = sin(pi*x)*sin(pi*y)
 eta_y = x*y*(x-1)*(y-1)
-u_x = 4*(x**3)*(y**4)*((x-1)**4)*((y-1)**4) + 4*(x**4)*(y**4)*((x-1)**3)*((y-1)**4)
-u_y = 4*(x**4)*(y**3)*((x-1)**4)*((y-1)**4) + 4*(x**4)*(y**4)*((x-1)**4)*((y-1)**3)
+# u_x = 4*(x**3)*(y**4)*((x-1)**4)*((y-1)**4) + 4*(x**4)*(y**4)*((x-1)**3)*((y-1)**4)
+# u_y = 4*(x**4)*(y**3)*((x-1)**4)*((y-1)**4) + 4*(x**4)*(y**4)*((x-1)**4)*((y-1)**3)
+
+# eta_x = sin(pi*x)**2 * sin(pi*y)
+# eta_y = -sin(pi*x) * sin(pi*y)**2
+u_x = sin(pi*x) * cos(pi*y)
+u_y = -cos(pi*x) * sin(pi*y)
+# exact_p =cos(pi*x) * cos(pi*y) + 2
+
 exact_eta = CF((eta_x, eta_y))
 exact_u = CF((u_x,u_y))
 exact_p = sin(pi*(x-y))
+# exact_p = eta_x = sin(pi*x)*sin(pi*y)
 
 # strain tensor
 epsilon_xx = eta_x.Diff(x)
@@ -143,13 +162,12 @@ sigma_xy = 2*mu*epsilon_xy
 f_x = - (sigma_xx.Diff(x) + sigma_xy.Diff(y))
 f_y = - (sigma_xy.Diff(x) + sigma_yy.Diff(y))
 
-fm_x = K*u_x-2*nu*(u_x.Diff(x).Diff(x)+u_x.Diff(y).Diff(y)) + exact_p.Diff(x)
-fm_y = K*u_y-2*nu*(u_y.Diff(x).Diff(x)+u_y.Diff(y).Diff(y)) + exact_p.Diff(y)
+fm_x = K*u_x-nu*(u_x.Diff(x).Diff(x)+u_x.Diff(y).Diff(y)) + exact_p.Diff(x)
+fm_y = K*u_y-nu*(u_y.Diff(x).Diff(x)+u_y.Diff(y).Diff(y)) + exact_p.Diff(y)
 
 # 向量形式
 fe = CF((f_x, f_y))
-fm = CF((fm_x, fm_x))
-# fp = (s0*exact_p+alpha*(eta_x.Diff(x)+eta_y.Diff(y))).Diff(t) + u_x.Diff(x) + u_y.Diff(y)
+fm = CF((fm_x, fm_y))
 fp = s0*exact_p+alpha*(eta_x.Diff(x)+eta_y.Diff(y)) + u_x.Diff(x) + u_y.Diff(y)
 
 etaD = exact_eta
@@ -160,8 +178,8 @@ results = []
 
 for k in range(2, 6):
     h0 = 1/2**k
-    error_eta, error_u, error_p, ndof = solve_biotBrinkman_steady_dg(h, order_eta, order_u,order_p, fe, fm,fp, exact_eta, exact_u, exact_p, \
-                                                                               mu,lam,beta_eta,beta_u,alpha,K)
+    error_eta, error_u, error_p, ndof = solve_biotBrinkman_steady_dg(h0, order_eta, order_u,order_p, fe, fm,fp, exact_eta, exact_u, exact_p, \
+                                                                               mu,lam,beta_eta,beta_u,gamma_p,alpha,K)
     results.append((h0,ndof,error_eta,error_u,error_p))
 
 print_convergence_table(results)
